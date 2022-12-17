22f891438b169be983202672da678b8566d6977c
Apollo71 <apollo71@mail.bg>
2022-12-17 21:03:46 +0200

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './core/error/error.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: MainComponent },
   { path: 'not-found', component: PageNotFoundComponent },
-  {path: 'error', component: ErrorComponent},
   // {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   // {path: 'theme', loadChildren: () => import('./themes/theme.module').then(m => m.ThemeModule)},
   { path: '**', redirectTo: '/not-found' },
